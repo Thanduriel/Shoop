5a52d610d1cb0ceb7005695c17d054ef53d2dda0
Thanduriel <rojendersie@alice.de>
2019-07-09 11:40:44 +0200

    # fixed some gcc warnings

--- src/gameplay/elements/lifetimecomponent.hpp
+++ src/gameplay/elements/lifetimecomponent.hpp
@@ -11,8 +11,8 @@ namespace Game {
 
 		void Process(float _deltaTime) override;
 
-		float GetTimeLeft() const { m_timeLeft; }
-		float GetTimeMax() const { m_timeMax; }
+		float GetTimeLeft() const { return m_timeLeft; }
+		float GetTimeMax() const { return m_timeMax; }
 	private:
 		const float m_timeMax;
 		float m_timeLeft;
